0cc3b1375e1244347e76199b2520ec2173515d52
Yurij <valetkrest@gmail.com>
2023-09-15 23:40:48 +0300

    rework findAllCategoryAndSub

--- server/queries/findAllCategoryAndSub.js
+++ server/queries/findAllCategoryAndSub.js
@@ -1,9 +1,24 @@
 "use strict";
-const { Category, Subcategory } = require("../db_schema/models");
+const {
+  Category,
+  Subcategory,
+  Product,
+  Sequelize,
+} = require("../db_schema/models");
 
 const findAllCategoryAndSub = async (isAdmin) => {
   const whereDisabled = isAdmin ? {} : { disabled: false };
   const attributeDisabled = isAdmin ? ["disabled", "imageBannerName"] : [];
+  const count = isAdmin
+    ? [
+        [
+          Sequelize.literal(
+            `CAST(COUNT("subcategories->products"."productId") AS INTEGER)`
+          ),
+          "productCount",
+        ],
+      ]
+    : [];
 
   try {
     const categoryAndSub = await Category.findAll({
@@ -12,11 +27,28 @@ const findAllCategoryAndSub = async (isAdmin) => {
       include: {
         model: Subcategory,
         as: "subcategories",
-        attributes: ["subcategoryId", "name", "linkKey", ...attributeDisabled],
+        attributes: [
+          "subcategoryId",
+          "name",
+          "linkKey",
+          ...attributeDisabled,
+          ...count,
+        ],
+        required: !isAdmin,
         where: { ...whereDisabled },
-        required: false,
+        order: [["name", "ASC"]],
+        include: {
+          model: Product,
+          as: "products",
+          where: { ...whereDisabled },
+          attributes: [],
+          required: !isAdmin,
+        },
       },
+      group: ["Category.categoryId", "subcategories.subcategoryId"],
+      order: [["name", "ASC"]],
     });
+
     return categoryAndSub;
   } catch (error) {
     throw new Error(error);
