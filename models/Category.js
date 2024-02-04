dff74a15fdaf9566721a3fb68669cf89bf5ee626
rryaboshtan <rrvworkroman@gmail.com>
2024-02-04 23:54:16 +0200

    category and subcategory uniqueness

--- models/Category.js
+++ models/Category.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING(64),
         allowNull: false,
+        unique: true,
       },
       // description: {
       //   type: DataTypes.STRING(500),
--- models/Subcategory.js
+++ models/Subcategory.js
@@ -34,8 +34,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING(64),
         allowNull: false,
-        // unique: true,
-        defaultValue: '',
+        unique: true,
       },
       // parentId: {
       //   allowNull: true,
