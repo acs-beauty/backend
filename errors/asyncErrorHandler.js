855844ab2a76557bb2704ac31118c4a1b321c8e6
rryaboshtan <rrvworkroman@gmail.com>
2024-02-10 18:45:11 +0200

    add 500 error handler

--- errors/asyncErrorHandler.js
+++ errors/asyncErrorHandler.js
@@ -3,8 +3,11 @@ const ApiError = require('./ApiError')
 const asyncErrorHandler = func => {
   return (req, res, next) => {
     func(req, res, next).catch(err => {
-      console.log(err)
-      next(ApiError.badRequest(err.errors))
+      if (err instanceof Error) {
+        next(ApiError.badRequest(err.errors))
+      } else {
+        next(ApiError.internalServerError(err.errors))
+      }
     })
   }
 }
