d4d4b34893c4ea1ae5b6a39b617f0758b8694f6e
rryaboshtan <rrvworkroman@gmail.com>
2024-03-14 20:45:10 +0200

    delete secure: true

--- utils/setTokens.js
+++ utils/setTokens.js
@@ -4,8 +4,8 @@ const setTokens = (res, id) => {
   const accessToken = jwt.sign({ id }, process.env.ACCESS_SECRET, { expiresIn: '15m' })
   const refreshToken = jwt.sign({ id }, process.env.REFRESH_SECRET, { expiresIn: '60d' })
 
-  res.cookie('accessToken', accessToken, { maxAge: 15 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true })
-  res.cookie('refreshToken', refreshToken, { maxAge: 60 * 24 * 3600 * 1000, httpOnly: true, sameSite: 'none', secure: true })
+  res.cookie('accessToken', accessToken, { maxAge: 15 * 60 * 1000, httpOnly: true, sameSite: 'none' })
+  res.cookie('refreshToken', refreshToken, { maxAge: 60 * 24 * 3600 * 1000, httpOnly: true, sameSite: 'none' })
 
   return accessToken
 }
