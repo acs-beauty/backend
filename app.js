c9f85b82376a675ff9d593977e3d695b330e68de
Roman <rrvwork27@gmail.com>
2024-04-06 22:20:56 +0300

    add localhost:3000

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const swaggerJSDoc = require('swagger-jsdoc')
 
 const app = express()
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173', sameSite: 'none' }))
+app.use(cors({ credentials: true, origin: ['http://localhost:5173', 'http://localhost:3000'], sameSite: 'none' }))
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*')
 //   res.header('Access-Control-Allow-Credentials', true)
